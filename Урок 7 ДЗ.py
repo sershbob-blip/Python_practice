11a33e1fd659cf4ac8d3aaa13e39adee110f9791
sershbob-blip <sershbob@gmail.com>
2026-09-07 19:58:54 +0300

    1

--- Урок 7 ДЗ.py
+++ Урок 7 ДЗ.py
@@ -1,5 +1,6 @@
+'''
 ДЗ
-арсинг CSV строк
+Парсинг CSV строк
 Задача:
 Разработать функции для работы с CSV-строками:
 
@@ -23,7 +24,39 @@ values = 'Иван,"Петров, Сергеевич",25,"Москва, цент
 Кавычки экранируют специальные символы
 
 Обработать пустые значения
+'''
 
+# csv_line = 'Имя,"Фамилия, Отчество",Возраст,Город'
+# values = 'Иван,"Петров, Сергеевич",25,"Москва, центр"'
+
+# def prepare_line(line:str) -> list[str]:
+#     result = []
+#     current = [] 
+#     inside_quotes = False
+
+#     for char in line:
+#         if char == '"':
+#             inside_quotes = not inside_quotes
+#         elif char == ',' and not inside_quotes:
+#             result.append(''.join(current).strip())
+#             current  = []
+#         else:
+#             current.append(char)
+#     result.append(''.join(current).strip())
+#     return result
+
+# def parsing_csv (csv_line:str, values:str) -> dict:
+#     fst_column = prepare_line(csv_line)
+#     snd_column = prepare_line(values)
+#     if len(csv_line) < len(values):
+#         snd_column.extend(['']*(len(fst_column) - len(snd_column)))
+#     result = dict(zip(fst_column,snd_column))
+#     return result
+
+# print(parsing_csv(csv_line, values)) 
+   
+
+'''
 ДЗ
 Задача:
 Разработать функции для работы с query-параметрами URL:
@@ -41,7 +74,35 @@ values = 'Иван,"Петров, Сергеевич",25,"Москва, цент
 python
 url = 'https://example.com?param1=value1&param2=value2&param1=value3'
 encoded_url = 'https://example.com?param=%D0%B7%D0%BD%D0%B0%D1%87%D0%B5%D0%BD%D0%B8%D0%B5'
+'''
+# url = 'https://example.com?param1=value1&param2=value2&param1=value3'
+# encoded_url = 'https://example.com?param=%D0%B7%D0%BD%D0%B0%D1%87%D0%B5%D0%BD%D0%B8%D0%B5'
 
+# def url_params(url:str):
+#     if '?' not in url:
+#         return {}
+    
+#     query = url.split('?',1)[1]
+
+#     params = {}
+    
+#     for pair in query.split('&'):
+#         if '=' in pair:
+#             key, value = pair.split('=',1)
+#         else:
+#             key, value = pair, ''
+#         params.setdefault(key,[]).append(value)
+    
+#     return params
+
+# from urllib.parse import urlparse, parse_qs, unquote
+# decoded = unquote(encoded_url) #https://example.com?param=значение
+# parsed = urlparse(url)
+# params = parse_qs(decoded.query) #{'param1': ['value1', 'value3'], 'param2': ['value2']}
+
+# print(params)
+
+'''
 ДЗ
  Формирование URL параметров
 Задача:
@@ -57,14 +118,22 @@ encoded_url = 'https://example.com?param=%D0%B7%D0%BD%D0%B0%D1%87%D0%B5%D0%BD%D0
 
 Исходные данные:
 
-python
-params = {
-    'search': 'значение',
-    'page': 2,
-    'tags': ['python', 'programming'],
-    'sort': 'asc'
-}
+'''
+# from urllib.parse import urlencode
+# params = {
+#     'search': 'значение',
+#     'page': 2,
+#     'tags': ['python', 'programming'],
+#     'sort': 'asc'
+# }
 
+# query = urlencode(params, doseq=True)
+
+# print(query)
+
+# #написать функцию
+
+'''
 ДЗ
 Задача:
 Создайте JSON-объекты для следующих сценариев:
@@ -78,6 +147,10 @@ params = {
 Пример:
 
 python
+'''
+
+import json
+
 # Создайте JSON-объект студента
 student = {
     "name": "Иван",
@@ -93,3 +166,15 @@ book = {
     "year": 1869,
     "pages": 1225
 }
+data = {
+    "name": "Анна",
+    "age": 21,
+    "courses": ["Python", "SQL"],
+    "active": True
+}
+
+book_jsn = json.dumps(book, ensure_ascii=False)
+print(book_jsn)
+
+
+credentials = json.loads(os.getenv("gs_key"))
